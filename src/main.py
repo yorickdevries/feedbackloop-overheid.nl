15c6f89609fb79ced41fd9c6e9cf8839dac6bb8e
Yorick de Vries <yvr@netcompany.com>
2023-11-10 12:29:57 +0100

    make output

--- src/main.py
+++ src/main.py
@@ -10,10 +10,16 @@ def make_counts(listtocount, field):
     for elem in listtocount:
         outputdict[elem[field]] += 1
     
-    print(outputdict)
-    
+    return outputdict
 
-if __name__ == '__main__':
+def dict_to_list(outputdict):
+    outputlist = []
+    for elem in outputdict:
+        outputlist.append({"name": elem, "count": outputdict[elem]})
+    outputlist.sort(key=lambda x: x["count"], reverse=True)
+    return outputlist
+    
+def contacts_1400():
     
     objlist = []
     
@@ -51,3 +57,85 @@ if __name__ == '__main__':
     #make_counts(objlist, "pageTitle")
     #make_counts(objlist, "forward")
     #make_counts(objlist, "forwardCode")
+
+def piwik_events():
+    
+    objlist = []
+    
+    with open('../Data/piwik_events.csv', newline='\n', encoding="utf8") as csvfile:
+        header = ["pw_session_id",
+                    "pw_event_id",
+                    "pw_visitor_id",
+                    "pw_timestamp",
+                    "pw_local_hour",
+                    "pw_is_exit",
+                    "pw_is_entry",
+                    "pw_event_type",
+                    "pw_event_url",
+                    "pw_event_title",
+                    "pw_outlink_url",
+                    "pw_download_url",
+                    "pw_search_keyword",
+                    "pw_search_category",
+                    "pw_search_results_count",
+                    "pw_custom_event_category",
+                    "pw_custom_event_action",
+                    "pw_custom_event_name",
+                    "pw_custom_event_value",
+                    "pw_previous_event_url",
+                    "pw_previous_event_title",
+                    "pw_next_event_url",
+                    "pw_next_event_title",
+                    "pw_event_index",
+                    "pw_page_view_index",
+                    "pw_time_on_page",
+                    "pw_date",
+                    "page_type",
+                    "ministerie",
+                    "hoofdonderwerp"
+                    ]
+        reader = csv.reader(csvfile, delimiter=';')
+        headerdone = False
+        
+        for row in reader:
+            if not headerdone:
+                headerdone = True
+            else:
+                obj = {}
+                for i in range(len(header)):
+                    obj[header[i]] = row[i]
+                objlist.append(obj)
+    
+    outputdict = make_counts(objlist, "hoofdonderwerp")
+    countlist = dict_to_list(outputdict)
+    
+    for elem in countlist:
+        print(str(elem["name"]) + "\t" + str(elem["count"]))
+    
+    #make_counts(objlist, "time")
+    #make_counts(objlist, "channel")
+    #make_counts(objlist, "ministry")
+    #make_counts(objlist, "subject")
+    #make_counts(objlist, "contactReason")
+    #make_counts(objlist, "outcome")
+    #make_counts(objlist, "pageTitle")
+    #make_counts(objlist, "forward")
+    #make_counts(objlist, "forwardCode")
+
+if __name__ == '__main__':
+    #contacts_1400()
+    piwik_events()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
