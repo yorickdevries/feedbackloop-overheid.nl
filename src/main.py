22b15171933f98021a074c7df49b039f822cb32f
Yorick de Vries <yvr@netcompany.com>
2023-11-10 15:33:05 +0100

    add networking

--- src/main.py
+++ src/main.py
@@ -1,4 +1,17 @@
 import csv
+import networkx as nx
+
+def make_double_counts(listtocount, field1, field2):
+    outputdict = {}
+    
+    for elem in listtocount:
+        if not (elem[field1], elem[field2]) in outputdict:
+            outputdict[(elem[field1], elem[field2])] = 0
+    
+    for elem in listtocount:
+        outputdict[(elem[field1], elem[field2])] += 1
+    
+    return outputdict
 
 def make_counts(listtocount, field):
     outputdict = {}
@@ -18,7 +31,18 @@ def dict_to_list(outputdict):
         outputlist.append({"name": elem, "count": outputdict[elem]})
     outputlist.sort(key=lambda x: x["count"], reverse=True)
     return outputlist
-    
+
+def print_list(pl):
+    counts = 0
+    for elem in pl:
+        counts += 1
+        if elem["name"][0] == "WhatsApp":
+            if counts > 100:
+                return
+            else:
+                print(str(elem["name"][1]) + "\t" + str(elem["count"]))
+
+
 def contacts_1400():
     
     objlist = []
@@ -45,7 +69,7 @@ def contacts_1400():
                 obj = {}
                 for i in range(len(header)):
                     obj[header[i]] = row[i]
-                    objlist.append(obj)
+                objlist.append(obj)
     
     #make_counts(objlist, "date")
     #make_counts(objlist, "time")
@@ -57,6 +81,14 @@ def contacts_1400():
     #make_counts(objlist, "pageTitle")
     #make_counts(objlist, "forward")
     #make_counts(objlist, "forwardCode")
+    
+    outputdict = make_counts(objlist, "channel")
+    countlist = dict_to_list(outputdict)
+    print_list(countlist)
+    
+    outputdict = make_double_counts(objlist, "channel", "subject")
+    countlist = dict_to_list(outputdict)
+    print_list(countlist)
 
 def piwik_events():
     
@@ -106,11 +138,11 @@ def piwik_events():
                     obj[header[i]] = row[i]
                 objlist.append(obj)
     
-    outputdict = make_counts(objlist, "hoofdonderwerp")
-    countlist = dict_to_list(outputdict)
+    #outputdict = make_counts(objlist, "hoofdonderwerp")
+    #countlist = dict_to_list(outputdict)
     
-    for elem in countlist:
-        print(str(elem["name"]) + "\t" + str(elem["count"]))
+    #for elem in countlist:
+    #    print(str(elem["name"]) + "\t" + str(elem["count"]))
     
     #make_counts(objlist, "time")
     #make_counts(objlist, "channel")
@@ -121,6 +153,79 @@ def piwik_events():
     #make_counts(objlist, "pageTitle")
     #make_counts(objlist, "forward")
     #make_counts(objlist, "forwardCode")
+    print(len(objlist))
+    
+    
+    nodes = {}
+    
+    field1 = "pw_previous_event_url"
+    field2 = "pw_next_event_url"
+    for elem in objlist:
+        if not elem[field1] in nodes:
+            nodes[elem[field1]] = 0
+
+        if not elem[field2] in nodes:
+            nodes[elem[field2]] = 0
+    
+    for elem in objlist:
+        nodes[elem[field1]] += 1
+        nodes[elem[field2]] += 1
+    
+    print(len(nodes))
+    
+    #outputdict = make_counts(objlist, "pw_previous_event_url")
+    #outputdict2 = make_counts(objlist, "pw_next_event_url")
+    
+    #for obj in objlist:
+    #    print("a")
+    
+    print("Constructing graph")
+    
+    
+    G_nodes = []
+    # make a node for every gene
+    for node in nodes:
+        G_nodes.append(node)
+    
+    # the #occurences of the connection should be higher than the threshold to be incorporated in the graph
+    # 1 or 2
+    threshold = 100
+    G_edges_orig = {}
+    
+    for elem in objlist:
+        if not (elem[field1], elem[field2]) in G_edges_orig:
+            G_edges_orig[(elem[field1], elem[field2])] = 0
+    
+    for elem in objlist:
+        G_edges_orig[(elem[field1], elem[field2])] += 1
+    
+    print(len(G_edges_orig))
+    
+    G_edges = []
+    
+    for elem in G_edges_orig:
+        weigth =  G_edges_orig[elem]
+        if weigth > threshold:
+            G_edges.append((elem[0], elem[1], weigth))
+    
+    # create a graph from the distance matrix
+    G = nx.DiGraph()
+    G.add_nodes_from(G_nodes)
+    G.add_weighted_edges_from(G_edges)
+    print("Graph made:", len(G.nodes), len(G.edges))
+    
+    # remove all isolated genes
+    G.remove_nodes_from(list(nx.isolates(G)))
+    
+    print("Graph made:", len(G.nodes), len(G.edges))
+    
+    
+    
+    
+    
+    
+    
+    
 
 if __name__ == '__main__':
     #contacts_1400()
